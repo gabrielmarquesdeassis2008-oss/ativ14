e9bdae3fbb046904e39b1e7600be84e36b0ed11e
gabrielmarquesdeassis2008 <gabrielmarquesdeassis2008@gmail.com>
2025-12-04 10:43:26 -0300

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,11 +1,5 @@
-let a=10 
-let b=5
-let c=0
-c=a+b
-document.writeln(c)
-c=a-b
-document.writeln(c)
-c=a*b
-document.writeln(c)
-c=a/b
-document.writeln(c)
+let i=0
+while(i<=100){
+    document.writeln(i)
+    i--
+}
